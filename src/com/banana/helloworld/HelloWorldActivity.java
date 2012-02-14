427e53f2a053f97980b569128deb374a4ace7293
piao <piao.polar@gmail.com>
2012-02-15 03:59:44 +0800

    创建一个简单view

--- src/com/banana/helloworld/HelloWorldActivity.java
+++ src/com/banana/helloworld/HelloWorldActivity.java
@@ -1,13 +1,18 @@
 package com.banana.helloworld;
 
 import android.app.Activity;
+
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HelloWorldActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+     //   setContentView(R.layout.main);
+        TextView tv = new TextView(this);
+        tv.setText("This is a textview");
+        setContentView(tv);
     }
 }
