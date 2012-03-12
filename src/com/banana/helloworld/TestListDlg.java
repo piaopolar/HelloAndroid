db4c77004bc44718717dccd492159a340a9fc6a5
piao <piao.polar@gmail.com>
2012-03-13 03:53:09 +0800

    网络恢复，找了篇文章来看了下，可以正常显示了 http://blog.csdn.net/redoffice/article/details/6666185

--- src/com/banana/helloworld/TestListDlg.java
+++ src/com/banana/helloworld/TestListDlg.java
@@ -1,5 +1,9 @@
 package com.banana.helloworld;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
@@ -8,11 +12,14 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 
-public class TestListDlg extends ListActivity {
+public class TestListDlg extends Activity {
 	
+//	private ListView listView;
+
 	static final String[] COUNTRIES = new String[] {    "Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra",    "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina",};
 	/** Called when the activity is first created. */
 	@Override
@@ -20,18 +27,16 @@ public class TestListDlg extends ListActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.test_list_dlg);
 
+		ListView listView  = (ListView)findViewById(R.id.listResult);  
+		  
+        listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, COUNTRIES));  
 
-//		ListView list=new ListView(this);
-//		ArrayAdapter<String> arrayadapter =new ArrayAdapter<String>(this, android.R.layout.test_list_dlg,COUNTRIES);
-//		list.setAdapter(arrayadapter);
-//		list.setOnItemClickListener(this);
-//
-//		findViewById(R.id.btnBack).setOnClickListener(new OnClickListener() {
-//			public void onClick(View v) {
-//				Intent intent = new Intent(TestListDlg.this,
-//						HelloWorldActivity.class);
-//				startActivity(intent);
-//			}
-//		});
+		findViewById(R.id.btnBack).setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(TestListDlg.this,
+						HelloWorldActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 }
