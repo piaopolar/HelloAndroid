910963175b2f650ad55a4622a79f056310c49c7d
piao <piao.polar@gmail.com>
2012-03-23 03:36:15 +0800

    a list grid demo

--- gen/com/banana/helloworld/R.java
+++ gen/com/banana/helloworld/R.java
@@ -14,21 +14,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Back2First=0x7f050009;
-        public static final int LinearLayout1=0x7f05000b;
+        public static final int Back2First=0x7f05000b;
+        public static final int HorizontalScrollView01=0x7f050009;
+        public static final int LinearLayout1=0x7f05000d;
+        public static final int ListView01=0x7f05000a;
         public static final int Next=0x7f050008;
         public static final int Pre=0x7f050007;
         public static final int RelativeLayout1=0x7f050005;
         public static final int Text=0x7f050006;
-        public static final int ToList=0x7f05000a;
+        public static final int ToList=0x7f05000c;
         public static final int answer1=0x7f050001;
         public static final int answer2=0x7f050003;
-        public static final int btnBack=0x7f050011;
-        public static final int btnSearch=0x7f05000f;
-        public static final int button1=0x7f05000c;
-        public static final int edtKeyWord=0x7f05000e;
-        public static final int linearLayout2=0x7f05000d;
-        public static final int listResult=0x7f050010;
+        public static final int btnBack=0x7f050013;
+        public static final int btnSearch=0x7f050011;
+        public static final int button1=0x7f05000e;
+        public static final int edtKeyWord=0x7f050010;
+        public static final int linearLayout2=0x7f05000f;
+        public static final int listResult=0x7f050012;
         public static final int price1=0x7f050002;
         public static final int price2=0x7f050004;
         public static final int question=0x7f050000;
--- src/com/banana/helloworld/CDlgMaterial.java
+++ src/com/banana/helloworld/CDlgMaterial.java
@@ -1,4 +1,5 @@
 package com.banana.helloworld;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -6,12 +7,65 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
+import com.testMyListView.TableAdapter;
+import com.testMyListView.TableAdapter.TableCell;
+import com.testMyListView.TableAdapter.TableRow;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.Toast;
+
+/**
+ * @author hellogv
+ */
 
 public class CDlgMaterial extends Activity {
-   @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.material);
-        
-    }
+	/** Called when the activity is first created. */
+	ListView lv;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.material);
+		this.setTitle("ListView自适应实现表格---hellogv");
+		lv = (ListView) this.findViewById(R.id.ListView01);
+		ArrayList<TableRow> table = new ArrayList<TableRow>();
+		TableCell[] titles = new TableCell[5];// 每行5个单元
+		int width = this.getWindowManager().getDefaultDisplay().getWidth()
+				/ titles.length;
+		// 定义标题
+		for (int i = 0; i < titles.length; i++) {
+			titles[i] = new TableCell("标题" + String.valueOf(i), width + 8 * i,
+					LayoutParams.FILL_PARENT, TableCell.STRING);
+		}
+		table.add(new TableRow(titles));
+		// 每行的数据
+		TableCell[] cells = new TableCell[5];// 每行5个单元
+		for (int i = 0; i < cells.length - 1; i++) {
+			cells[i] = new TableCell("No." + String.valueOf(i),
+					titles[i].width, LayoutParams.FILL_PARENT, TableCell.STRING);
+		}
+		cells[cells.length - 1] = new TableCell(android.R.drawable.ic_media_play,
+				titles[cells.length - 1].width, LayoutParams.WRAP_CONTENT,
+				TableCell.IMAGE);
+		// 把表格的行添加到表格
+		for (int i = 0; i < 12; i++)
+			table.add(new TableRow(cells));
+		TableAdapter tableAdapter = new TableAdapter(this, table);
+		lv.setAdapter(tableAdapter);
+		lv.setOnItemClickListener(new ItemClickEvent());
+	}
+
+	class ItemClickEvent implements AdapterView.OnItemClickListener {
+		public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+				long arg3) {
+			Toast.makeText(CDlgMaterial.this,
+					"选中第" + String.valueOf(arg2) + "行", 500).show();
+		}
+	}
 }
